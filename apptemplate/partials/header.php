b776ec74b95bd7fe58673dac21f2f37e39bcd7fd
lecongngan <47780129+lecongngan@users.noreply.github.com>
2019-06-13 16:44:35 +0700

    Add files via upload

---
+++ apptemplate/partials/header.php
@@ -0,0 +1,99 @@
+
+<header id="header">
+			<!-- Nav -->
+			<div id="nav">
+				<!-- Main Nav -->
+				<div id="nav-fixed">
+					<div class="container">
+						<!-- logo -->
+						<div class="nav-logo">
+							<a href="index.php" class="logo"><img src="assets/img/logo.png" alt=""></a>
+						</div>
+						<!-- /logo -->
+
+						<!-- nav -->
+						<ul class="nav-menu nav navbar-nav">
+							<li><a href="pages/category.php">News</a></li>
+							<li><a href="pages/category.php">Popular</a></li>
+							<li class="cat-1"><a href="pages/category.php">Web Design</a></li>
+							<li class="cat-2"><a href="pages/category.php">JavaScript</a></li>
+							<li class="cat-3"><a href="pages/category.php">Css</a></li>
+							<li class="cat-4"><a href="pages/category.php">Jquery</a></li>
+						</ul>
+						<!-- /nav -->
+
+						<!-- search & aside toggle -->
+						<div class="nav-btns">
+							<button class="aside-btn"><i class="fa fa-bars"></i></button>
+							<button class="search-btn"><i class="fa fa-search"></i></button>
+							<div class="search-form">
+								<input class="search-input" type="text" name="search" placeholder="Enter Your Search ...">
+								<button class="search-close"><i class="fa fa-times"></i></button>
+							</div>
+						</div>
+						<!-- /search & aside toggle -->
+					</div>
+				</div>
+				<!-- /Main Nav -->
+
+				<!-- Aside Nav -->
+				<div id="nav-aside">
+					<!-- nav -->
+					<div class="section-row">
+						<ul class="nav-aside-menu">
+							<li><a href="index.php">Home</a></li>
+							<li><a href="pages/about.php">About Us</a></li>
+							<li><a href="#">Join Us</a></li>
+							<li><a href="#">Advertisement</a></li>
+							<li><a href="pages/contact.php">Contacts</a></li>
+						</ul>
+					</div>
+					<!-- /nav -->
+
+					<!-- widget posts -->
+					<div class="section-row">
+						<h3>Recent Posts</h3>
+						<div class="post post-widget">
+							<a class="post-img" href="pages/blog.php"><img src="assets/img/widget-2.jpg" alt=""></a>
+							<div class="post-body">
+								<h3 class="post-title"><a href="pages/blog.php">Pagedraw UI Builder Turns Your Website Design Mockup Into Code Automatically</a></h3>
+							</div>
+						</div>
+
+						<div class="post post-widget">
+							<a class="post-img" href="pages/blog.php"><img src="assets/img/widget-3.jpg" alt=""></a>
+							<div class="post-body">
+								<h3 class="post-title"><a href="pages/blog.php">Why Node.js Is The Coolest Kid On The Backend Development Block!</a></h3>
+							</div>
+						</div>
+
+						<div class="post post-widget">
+							<a class="post-img" href="pages/blog.php"><img src="assets/img/widget-4.jpg" alt=""></a>
+							<div class="post-body">
+								<h3 class="post-title"><a href="pages/blog.php">Tell-A-Tool: Guide To Web Design And Development Tools</a></h3>
+							</div>
+						</div>
+					</div>
+					<!-- /widget posts -->
+
+					<!-- social links -->
+					<div class="section-row">
+						<h3>Follow us</h3>
+						<ul class="nav-aside-social">
+							<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+							<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+							<li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+						</ul>
+					</div>
+					<!-- /social links -->
+
+					<!-- aside nav close -->
+					<button class="nav-aside-close"><i class="fa fa-times"></i></button>
+					<!-- /aside nav close -->
+				</div>
+				<!-- Aside Nav -->
+			</div>
+			<!-- /Nav -->
+		</header>
+		
