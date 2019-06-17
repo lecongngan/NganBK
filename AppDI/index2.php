70260e5901d6978416ded01635fc988ac5982169
lecongngan <47780129+lecongngan@users.noreply.github.com>
2019-06-17 23:58:41 +0700

    Add files via upload

---
+++ AppDI/index2.php
@@ -0,0 +1,58 @@
+<?php
+
+class Author {
+    private $firstname;
+    private $lastname;
+    /**
+     * Author constructor.
+     * @param $firstname
+     * @param $lastname
+     * Hàm khởi tạo construct
+     */
+    public function __construct($firstname,$lastname)
+    {
+        /**
+         * Gán 2 tham số truyền vào cho thuộc tính trong class
+         */
+        $this->firstname = $firstname;
+        $this->lastname = $lastname;
+    }
+    public function getFirstname(){
+        return $this->firstname;
+    }
+    /**
+     * @return mixed
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+}
+class Question {
+    private $author;
+    /**
+     *  Khởi tạo Thuộc tính author là 1 đối tượng của class khác
+     */
+    private $question;
+    public function __construct($question,Author $author)
+    {
+        $this->author = $author;
+        $this->question = $question;
+    }
+    /**
+     * @return Author
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+    /**
+     * @return mixed
+     */
+    public function getQuestion()
+    {
+        return $this->question;
+    }
+}
+$author = new Author("super", "admin");
+$question = new Question("How to creat Dependency Injection PHP ?", $author);
